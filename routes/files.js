145fd4c7558ef65826c556651ad433dde603be71
Eli Pavlov <pavlov.eli@gmail.com>
2025-09-29 21:30:23 +0300

    Update files.js

--- routes/files.js
+++ routes/files.js
@@ -1,8 +1,10 @@
-const express = require("express");
+// routes/files.js
+const express = require('express');
 const router = express.Router();
-const filesController = require("../controllers/files");
+const filesController = require('../controllers/files');
+const authMiddleware = require('../middleware/authToken');
 
-router.post('/upload', filesController.upload);
-router.post('/delete', filesController.delete);
+router.post('/presigned-url', authMiddleware.verifyAuthToken, filesController.generatePresignedUrl);
+router.post('/delete', authMiddleware.verifyAuthToken, filesController.delete);
 
-module.exports = router
+module.exports = router;
